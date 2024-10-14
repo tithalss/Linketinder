80a8417521cd88cde099006a5baddbda3aa4ba10
Thales de Paula Martins <contaprofissional7@outlook.com>
2024-10-14 16:25:40 -0300

    Reestruturação do frontend para o projeto de MVC - 14/10

--- frontend/src/components/login.ts
+++ frontend/src/components/login.ts
@@ -1,49 +1,10 @@
-function createLoginForm(): HTMLElement {
-    const container = document.createElement('div');
-    container.classList.add('login-container');
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("formulario-empresa") as HTMLFormElement;
 
-    const logo = document.createElement('img');
-    logo.src = '/Linketinder/frontend-ts/public/assets/Ltlogo.png';
-    logo.alt = 'Logo Linketinder';
-    logo.classList.add('login-logo');
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    const usernameInput = document.createElement('input');
-    usernameInput.type = 'text';
-    usernameInput.placeholder = 'EMAIL';
-
-    const passwordInput = document.createElement('input');
-    passwordInput.type = 'password';
-    passwordInput.placeholder = 'SENHA';
-
-    const loginButton = document.createElement('button');
-    loginButton.textContent = 'Entrar';
-
-    const registerAsCompanyButton = document.createElement('a');
-    registerAsCompanyButton.textContent = 'Cadastrar como Empresa';
-    registerAsCompanyButton.href = 'formularioEmpresa.html';
-
-    const registerAsCandidateButton = document.createElement('a');
-    registerAsCandidateButton.textContent = 'Cadastrar como Candidato';
-    registerAsCandidateButton.href = 'formularioCandidato.html';
-
-    container.appendChild(logo);
-    container.appendChild(usernameInput);
-    container.appendChild(passwordInput);
-    container.appendChild(loginButton);
-
-    const registerContainer = document.createElement('div');
-    registerContainer.classList.add('register-container');
-    registerContainer.appendChild(registerAsCompanyButton);
-    registerContainer.appendChild(registerAsCandidateButton);
-
-    container.appendChild(registerContainer);
-
-    return container;
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    const app = document.querySelector('body');
-    if (app) {
-        app.appendChild(createLoginForm());
-    }
+        const email = (document.getElementById("email") as HTMLInputElement).value;
+        const senha = (document.getElementById("senha") as HTMLInputElement).value;
+    });
 });
